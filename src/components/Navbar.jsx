bda197a17ed54d2d4ae413117056d49bf06a5b4e
RedJyu <piotr.stezycki@protonmai.com>
2023-12-18 14:19:29 +0100

    up

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='nav-center'>
+        <h3>redux</h3>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
