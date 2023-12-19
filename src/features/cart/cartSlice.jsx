ef9b3706a63840406d35098811f820d1ee1eb573
RedJyu <piotr.stezycki@protonmai.com>
2023-12-19 13:52:08 +0100

    buttons working

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -15,9 +15,21 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemID = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemID);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
 export default cartSlice.reducer;
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
